ad41cca80c4a793eed41ff9b9d382cc55a7db291
BreakfastMatt <16mdlewis@gmail.com>
2017-10-11 23:27:40 +0200

    final touches

--- fuckingSpaceInvaders/fuckingSpaceInvaders/Bullet.cs
+++ fuckingSpaceInvaders/fuckingSpaceInvaders/Bullet.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+﻿
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,50 +12,6 @@ using System.Windows.Controls;
 
 namespace fuckingSpaceInvaders
 {
-    class Bullet
-    {
-
-
-        void BulletFire(Turtle Bull)
-        {
-
-        }
-        public static void UpdateBullet(Turtle Bull, double p, Label txt, double wind, Random rng) // p == power
-        {
-            Color k = Bull.ColorUnderTurtle;
-            wind = rng.Next(-30,30);
-            if (wind > 0) txt.Content = $"Wind:  {wind}km East";
-            else txt.Content = $"Wind:  {-1 * wind}km West";
-            while (k.ToString() == "#FF3F61FE" || k.ToString() == "#FFFFFFFF" || k.ToString() == "#FF000000" || k.ToString() == "#FF00A2E8" || k.ToString() == "#FF121214" || k.ToString() == "#FF12121E" || k.ToString() == "#FF121232") //Checking the bullet is in the air)
-            {//beautiful
-                k = Bull.ColorUnderTurtle;
-                if (Bull.Heading < 90 || (Bull.Heading > 180))
-                {
-                    Bull.Forward(p / 10);
-                    Bull.Heading += 2;
-                }
-                else
-                {
-                    Bull.Forward(p / 10);
-                    if (Bull.Position.X > 164 && Bull.Position.Y < 592)
-                    {
-                        
-                        MessageBox.Show("HIT!");
-                    }
-                }
-            }
-                
-                
-            }
-              
-            
-
-             
-        
-    }
-}
-=======
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -112,4 +68,3 @@ namespace fuckingSpaceInvaders
 
         }
     }
->>>>>>> d63a7e6a1e1b31228114c0b57f3809f0a2053335
